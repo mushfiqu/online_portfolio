b976eb7da292e2fd62b71788118a2112784efd94
mushfiqu <mushfiquenabi@gmail.com>
2023-06-23 17:39:54 -0600

    updating portfolio

--- react-portfolio/src/app/page.js
+++ react-portfolio/src/app/page.js
@@ -5,6 +5,7 @@ import { AiFillLinkedin } from "react-icons/ai";
 import { BsGithub } from "react-icons/bs";
 import { SiGmail } from "react-icons/si";
 import profile_pic from '../app/public/portfolio_photo.png'
+import background from '../app/public/portfolio_bg.jpg'
 
 export default function Home() {
   return (
@@ -15,8 +16,7 @@ export default function Home() {
       </head>
 
       <main className = "bg-gradient-to-r from-indigo-500 to-red-500 px-10">
-        <section className = "min-h-screen">
-          <nav className = "py-10 mb-12 flex justify-between">
+      <nav className = "p-10 mb-12 flex justify-between">
           <ul className = "flex items-center">
             <li>
               <SiGmail class = "text-red-700 hover:text-red-500"/>
@@ -28,14 +28,32 @@ export default function Home() {
             </li> 
           </ul>
           </nav>
-          <div className = "text-center p-10">
-            <h2 className = "text-4xl py-2 text-white  hover:text-5xl duration-700">Mushfique Un Nabi</h2>
-            <h3 class = "text-2xl py-2 hover:text-3xl duration-700"> Electrical Engineer</h3>
-            <p className = "text-md py-5 leading-8 text-gray-900">
-              Electrical engineer specializing in embedded systems design, firmware development,
-              and software development. 
-            </p>
+      <div
+        class="relative overflow-hidden rounded-lg bg-cover bg-no-repeat p-16 text-center flex items-center justify-center"
+        style={{backgroundImage: "url('...')"}}>
+          <Image src = {background} class = "rounded-3xl"/>
+        <div
+          class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+          style={{backgroundColor: "rgba(0, 0, 0, 0)"}}>
+          <div class="flex h-full items-center justify-center">
+            <div class="text-white">
+              <h2 class="mb-4 text-4xl font-semibold">Mushfique Un nabi</h2>
+              <h4 class="mb-6 text-xl font-semibold">Electrical Engineer</h4>
+              <a href="#about">
+              <button
+                type="button"
+                class="rounded border-2 border-neutral-50 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+                onClick="document.getElementById('about').scrollIntoView()">
+                About
+              </button>
+              </a>
+            </div>
           </div>
+        </div>
+      </div>
+        <section className = "min-h-screen">
           <div className = "text-5xl flex justify-center gap-16 py-3 text-blue-800">
             <a href = "https://www.linkedin.com/in/mushfique-un-nabi-11657226a/">
             <AiFillLinkedin class = "hover:animate-bounce"/>
@@ -44,10 +62,11 @@ export default function Home() {
             <BsGithub class = "hover:animate-bounce"/>
             </a>
           </div>
-          <div className='relative mx-auto w-30 h-30'>
+          <div className='relative mx-auto w-13 h-13'>
             <Image src = {profile_pic}/>
           </div>
         </section>
+        <div id='about'>
         <section>
           <div className = "shadow-lg rounded-lg p-10">
               <span class="relative flex h-3 w-3">
@@ -63,6 +82,7 @@ export default function Home() {
            </p>
           </div>
         </section>
+        </div>
         <section>
           <div className = "shadow-lg rounded-lg p-10">
             <div>
